4e1d324cc08a7a6ead8685db3c6cd6a40f9e21b9
JohnnHidalgo <johnna.7h@gmail.com>
2021-02-16 20:13:06 -0400

    add cooperativa finished

--- CeraviFrontend/src/app/view/cooperativa/addcooperativa/addcooperativa.component.ts
+++ CeraviFrontend/src/app/view/cooperativa/addcooperativa/addcooperativa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CreateCooperativa } from 'src/app/model/cooperativa';
+import { CooperativaService } from 'src/app/service/cooperativa/cooperativa.service';
 
 @Component({
   selector: 'app-addcooperativa',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddcooperativaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private service:CooperativaService) { }
 
+  cooperativa = new CreateCooperativa("","",new Date,true);
+
+  
+  createCooperativa(){
+    var dd = this.cooperativa.tx_date.getDate();
+    var mm = this.cooperativa.tx_date.getMonth()+1;
+    var yyyy = this.cooperativa.tx_date.getFullYear();
+    var date = yyyy+"-"+mm+"-"+dd;
+    this.cooperativa.tx_date=date;
+
+    this.service.createCooperativa(this.cooperativa)
+    .subscribe(data=>{
+      alert("Creación Exitosa");
+      this.router.navigate(["listcooperativa"]);
+    })
+  }
   ngOnInit(): void {
   }
+
+
 
 }
